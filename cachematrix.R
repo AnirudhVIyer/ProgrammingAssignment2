11b2dcc30ce50684d95aee95167b280ea9af44f2
AnirudhVIyer <anirudhiyer96@gmail.com>
2021-05-01 15:09:13 +0530

    Commit to cachematrix.R, with solution to PGA for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## making a function to set.get values of a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
+  myInverse <- NULL
+  ## setting values
+  setVal <- function(y){
+    x <<- y
+    myInverse <<- NULL   ## here we don't set the values of the inverse
+  }
+  
+  ##getting the matrix
+  getVal <- function()x  
+  ##setting inverse
+  setInv <- function(inverse) myInverse <<- inverse  
+  
+  ##getting inverse
+  getInv <- function() myInverse 
+  
+  list(setVal = setVal, getVal = getVal, setInv = setInv, getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## calculate the inverse of matrix it it was not calculated before
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInv()
+  if(!is.null(inverse)){
+    message("Inverse already calculated")
+    return(inverse)
+  }
+  ## inverse not calculated , has to be manually calculated
+  matrix1 <- x$getVal()
+  inverse <- solve(matrix1,...)
+  x$setInv(inverse)
+  return(inverse)
+        
+     
 }
+
+
+m
